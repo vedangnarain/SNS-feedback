76cff56e90b60a4bd408be20ef75d4d80fdba143
Vedang Narain <35129667+vedangnarain@users.noreply.github.com>
2019-12-08 20:33:54 -0500

    Seems to work on one recording

--- analysis.py
+++ analysis.py
@@ -87,7 +87,7 @@ eda = stats.zscore(eda)
 
 # plots EDA
 plt.figure(figsize = (8,8))
-ax1 = plt.subplot(6, 1, 1)
+ax1 = plt.subplot(7, 1, 1)
 plt.title('EDA (I = 1.0 A)')
 ax1.plot(time_eda, eda)
 #plt.xlabel('Seconds')
@@ -119,7 +119,7 @@ for i in range(epc_window_length, len(eda_increases_list)):
     epc_list.append(epc_value)
 
 # plots EPC    
-ax2 = plt.subplot(6, 1, 2, sharex = ax1)
+ax2 = plt.subplot(7, 1, 2, sharex = ax1)
 plt.title('EPC')  # epc
 ax2.plot(time_eda, epc_list)
 #plt.xlabel('Seconds')
@@ -127,14 +127,14 @@ plt.ylabel('z-score')
 plt.grid(True, alpha = 0.5)
 
 #==============================================================================
-#  IBI EXTRACTION
+# HEART RATE
 #==============================================================================
 
 # z-scores PPG
 ppg = stats.zscore(ppg)
 
 # plots PPG
-ax3 = plt.subplot(6, 1, 3, sharex = ax1)
+ax3 = plt.subplot(7, 1, 3, sharex = ax1)
 plt.title('PPG')
 ax3.plot(time_ppg, ppg)
 #plt.xlabel('Seconds')
@@ -148,23 +148,18 @@ dz_ppg = np.gradient(ppg)
 dz_peaks, _ = signal.find_peaks(dz_ppg, distance = 0.28 * fs, prominence = 0.1)
 
 # plots PPG gradient with peaks
-ax4 = plt.subplot(6, 1, 4, sharex = ax1)
+ax4 = plt.subplot(7, 1, 4, sharex = ax1)
 plt.title('PPG Gradient (with peaks)')
 ax4.plot(time_ppg, dz_ppg)
 ax4.plot(time_ppg[dz_peaks], dz_ppg[dz_peaks], "x")  # mark peaks, no offset needed after filtering
 plt.ylabel('d(z-score)/dt')
 plt.grid(True, alpha = 0.5)
 
-'''
-SECTION BELOW STILL NEEDS WORK
-'''
-
 # initialize empty list for IBIs
 ibi_list = [0]
 
 # generate list of IBIs for cropped signal
 cnt = 0
-
 for i in range(1, len(dz_peaks)):
     interval = (dz_peaks[i] - dz_peaks[i-1])/fs
     if interval < 1:  # if IBI is greater than or equal to 1 second, use previous value instead
@@ -172,14 +167,82 @@ for i in range(1, len(dz_peaks)):
     else:
         ibi_list.append(ibi_list[i-1])  
 
-# generates time axis for IBIs
-ibi_time_axis = np.cumsum(ibi_list)
+# converts IBIs from seconds/beat to bpm so that faster heart rate gives higher score
+bpm = []
+for x in ibi_list:
+    if x!=0:
+        ibi_reci = (1/x) * 60
+        bpm.append(ibi_reci)  
+    else:
+        bpm.append(0)  
+        
+# generates axis for the bpm list
+interp_time_peaks = dz_peaks/fs
+
+# interpolates bpm
+interp_bpm = np.interp(time_ppg, interp_time_peaks, bpm)
 
 # plots IBIs    
-ax5 = plt.subplot(6, 1, 5, sharex = ax1)
-plt.title('IBIs')
-ax5.plot(ibi_time_axis, ibi_list)
-plt.ylabel('seconds')
+ax5 = plt.subplot(7, 1, 5, sharex = ax1)
+plt.title('Heart Rate')
+ax5.plot(time_ppg, interp_bpm)
+plt.ylabel('bpm')
+plt.ylim(min(interp_bpm[1000:]), max(interp_bpm[1000:]))
+plt.grid(True, alpha = 0.5)
+
+# prepares list HR Increases (HRIs)
+hr_increases_list = [0]
+for i in range(1, len(interp_bpm)):
+    if interp_bpm[i] > interp_bpm[i-1]:
+        hr_increase = interp_bpm[i] - interp_bpm[i-1]
+        hr_increases_list.append(hr_increase)
+    else:
+        hr_increases_list.append(0)
+
+# specifies window length (5 seconds)
+hr_window_length = int(5*fs)
+
+# calculates starting index to leave space for 5-second buffer
+hr_start_index = hr_window_length - 1
+
+# prepares list of 5-second sums
+hri_list = [0] * (hr_start_index)  # intializes zeros for initial 5-second buffer period
+hri_list.append(sum(hr_increases_list[0:hr_window_length]))  # initializes first 5-second HRI by summing first 1000 values
+
+# generates the rest of the HRIs more efficiently
+for i in range(hr_window_length, len(hr_increases_list)):
+    hri_value = (hri_list[i - 1] - hr_increases_list[i - hr_window_length] + hr_increases_list[i])
+    hri_list.append(hri_value)
+
+# plots IBIs    
+ax6 = plt.subplot(7, 1, 6, sharex = ax1)
+plt.title('HR Positive Change')
+ax6.plot(time_ppg, hri_list)
+plt.ylabel('bpm')
+plt.ylim(min(hri_list[1500:]), max(hri_list[1500:]))
+plt.legend()
+plt.grid(True, alpha = 0.5)
+
+#==============================================================================
+#  WEIGHTED-SCORES GENERATION
+#==============================================================================
+
+# the overall scoring scale ranges from zero to the sum of the max. pulse score and the max. EPC score
+max_pulse_score = 1.5  # 30% weightage
+max_epc_score = 3.5  # 70% weightage
+
+# calculates scores
+pulse_scores = hri_list/max(hri_list) * max_pulse_score
+epc_scores = epc_list/max(epc_list) * max_epc_score
+scores = pulse_scores + epc_scores
+
+# plots IBIs    
+ax7 = plt.subplot(7, 1, 7, sharex = ax1)
+plt.title('Scores')
+#ax7.plot(time_ppg, pulse_scores, label='Pulse Score')
+#ax7.plot(time_eda, epc_scores, label='EPC Score')
+ax7.plot(time_eda, scores, label='Overall Score')
+plt.ylabel('score')
 #plt.legend()
 plt.grid(True, alpha = 0.5)
 
@@ -188,45 +251,5 @@ plt.xlabel('seconds')
 plt.subplots_adjust(hspace = 1)
 plt.show()
 
-
-'''
-NEXT STEPS: cumulative IBIs falls short of end because it makes up values. also, it ignores first peak time. fix.
-take window of 10-second IBI change (decreases instead of increases)
-
-see if IBI calibration (and score) is okay (try on more samples)
-
-'''
-
-#==============================================================================
-#  WEIGHTED-SCORES
-#==============================================================================
-
-'''
-scores = []
-
-loop
-score = (0.7*epc) + (0.3*ibi_changes)  # x/max * 5
-append (score)
-'''
-
 # prints execution time
 print("\n--- Execution Time: %s seconds ---" % (time.time() - start_time))
-
-# =============================================================================
-# NOTES
-# =============================================================================
-
-'''
-
- The EDA complex includes both
-background tonic (skin conductance level: SCL) and rapid phasic components (Skin
-Conductance Responses: SCRs) that result from sympathetic neuronal activity. EDA is
-arguably the most useful index of changes in sympathetic arousal that are tractable to
-emotional and cognitive states as it is the only autonomic psychophysiological variable that is
-not contaminated by parasympathetic activity. EDA has been closely linked to autonomic
-emotional and cognitive processing, and EDA is a widely used as a sensitive index of
-emotional processing and sympathetic activity.
-
-60 s baseline session. 
-
-'''
