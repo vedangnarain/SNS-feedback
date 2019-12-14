81ef30c9908bd254fb0f0265c87748c4ac8b20b8
Vedang Narain <35129667+vedangnarain@users.noreply.github.com>
2019-12-14 17:26:55 -0500

    Final version

--- analysis.py
+++ analysis.py
@@ -9,7 +9,9 @@ Created on Thu Oct  3 14:16:57 2019
 #==============================================================================
 
 # imports libraries
+from ast import literal_eval
 import matplotlib.pyplot as plt
+from matplotlib import cm
 import numpy as np
 import pandas as pd
 from scipy import signal, stats
@@ -36,19 +38,12 @@ def butterworth_bandpass_filter(unfiltered_signal, low_cut, high_cut, order):
     filtered_signal = signal.lfilter(b, a, unfiltered_signal)
     return filtered_signal
 
-# defines function to pass signal through matched filter
-def matched_filter(unfiltered_signal, template):
-    fir_coeff = template[::-1]
-    detected = signal.lfilter(fir_coeff, 1, unfiltered_signal)
-    detected = detected * detected  # squaring to improve SNR
-    return detected
-
 #==============================================================================
 # IMPORT AND PREPARE DATA
 #==============================================================================
 
 # imports raw data
-raw_data = pd.read_csv('/Users/Vedang/Desktop/Wearable Sensors/coolterm_ppg_eda_24_oct.txt', header=None)
+raw_data = pd.read_csv('coolterm_ppg_eda_24_oct.txt', header=None)
 
 # replaces NaN instances with zero
 raw_data = raw_data.fillna(0)
@@ -57,13 +52,45 @@ raw_data = raw_data.fillna(0)
 eda = raw_data.iloc[:, 0]
 ppg = raw_data.iloc[:, 1]
 
+'''
+# use this code for bad test data
+## imports EDA file and sews samples together
+#raw_eda = pd.read_csv('/Users/Vedang/Desktop/Wearable Sensors/test_data/Wearable_data_set__/Wearable sensor data_test_eda_1.csv', header=None)
+#raw_eda = raw_eda.fillna(0)
+#eda_list = []
+#for i in range(0, len(raw_eda)):
+#    eda_block = literal_eval(raw_eda.iloc[i,0])
+#    eda_list = eda_list + eda_block
+#eda = pd.Series(eda_list)
+#eda = eda.fillna(0)    
+#
+## imports PPG file and sews samples together
+#raw_ppg = pd.read_csv('/Users/Vedang/Desktop/Wearable Sensors/test_data/Wearable_data_set__/Wearable sensor data_test_ppg_1.csv', header=None)
+#raw_ppg = raw_ppg.fillna(0)
+#ppg_list = []
+#for i in range(0, len(raw_ppg)):
+#    ppg_block = literal_eval(raw_ppg.iloc[i,0])
+#    ppg_list.extend(ppg_block)
+##    ppg_list.append(ppg_block)    
+#ppg = pd.Series(ppg_list)
+#ppg = ppg.fillna(0)
+'''
+
+# reciprocal converts to siemens
+eda = eda.apply(lambda x: 1/x if x != 4 else 0)
+
+# specifies length of inital normalization and calbration periods (in seconds)
+normalization = 10  # change to 60 when using protocol in instructions
+calibration = 10  # change to 60 when using protocol in instructions
+recording_start = normalization + calibration
+
 # specifies desired sampling rate
 fs = 100
 
 # downsamples signals (assuming sampling rate is 100 Hz)
 downsampling_factor_eda = 100/fs
 downsampling_factor_ppg = 100/fs
-eda = 1/eda[::int(downsampling_factor_eda)]  # reciprocal converts to siemens
+eda = eda[::int(downsampling_factor_eda)]
 ppg = ppg[::int(downsampling_factor_ppg)]
 
 # offsets the original segments to minimize ripple
@@ -71,8 +98,8 @@ ppg = ppg - ppg[0]
 eda = eda - eda[0]
 
 # filters signals to remove noise
-ppg = butterworth_bandpass_filter(ppg, 0.000000001, 2.5, 3)
-eda = butterworth_bandpass_filter(eda, 0.000000001, 1.5, 3)
+ppg = butterworth_bandpass_filter(ppg, 0.000000001, 2.5, 3) 
+eda = butterworth_bandpass_filter(eda, 0.000000001, 3, 3)  
 
 # prepares time axis for plots
 time_eda = np.linspace(0, len(eda)/fs, len(eda))
@@ -90,15 +117,14 @@ plt.figure(figsize = (8,8))
 ax1 = plt.subplot(7, 1, 1)
 plt.title('EDA (I = 1.0 A)')
 ax1.plot(time_eda, eda)
-#plt.xlabel('Seconds')
 plt.ylabel('z-score')
 plt.grid(True, alpha = 0.5)
 
 # specifies window length (10 seconds)
-epc_window_length = int(10*fs) #1000
+epc_window_length = int(10*fs)
 
 # calculates starting index to leave space for 10-second buffer
-epc_start_index = epc_window_length - 1 #999
+epc_start_index = epc_window_length - 1
 
 # prepares list of EDA increases
 eda_increases_list = [0]
@@ -120,9 +146,8 @@ for i in range(epc_window_length, len(eda_increases_list)):
 
 # plots EPC    
 ax2 = plt.subplot(7, 1, 2, sharex = ax1)
-plt.title('EPC')  # epc
+plt.title('EPC')
 ax2.plot(time_eda, epc_list)
-#plt.xlabel('Seconds')
 plt.ylabel('z-score')
 plt.grid(True, alpha = 0.5)
 
@@ -137,8 +162,8 @@ ppg = stats.zscore(ppg)
 ax3 = plt.subplot(7, 1, 3, sharex = ax1)
 plt.title('PPG')
 ax3.plot(time_ppg, ppg)
-#plt.xlabel('Seconds')
 plt.ylabel('z-score')
+plt.ylim(min(ppg[recording_start*fs:]), max(ppg[recording_start*fs:]))
 plt.grid(True, alpha = 0.5)
 
 # generates PPG gradient signal
@@ -152,6 +177,7 @@ ax4 = plt.subplot(7, 1, 4, sharex = ax1)
 plt.title('PPG Gradient (with peaks)')
 ax4.plot(time_ppg, dz_ppg)
 ax4.plot(time_ppg[dz_peaks], dz_ppg[dz_peaks], "x")  # mark peaks, no offset needed after filtering
+plt.ylim(min(dz_ppg[recording_start*fs:]), max(dz_ppg[recording_start*fs:]))
 plt.ylabel('d(z-score)/dt')
 plt.grid(True, alpha = 0.5)
 
@@ -187,7 +213,7 @@ ax5 = plt.subplot(7, 1, 5, sharex = ax1)
 plt.title('Heart Rate')
 ax5.plot(time_ppg, interp_bpm)
 plt.ylabel('bpm')
-plt.ylim(min(interp_bpm[1000:]), max(interp_bpm[1000:]))
+plt.ylim(min(interp_bpm[recording_start*fs:]), max(interp_bpm[recording_start*fs:]))
 plt.grid(True, alpha = 0.5)
 
 # prepares list HR Increases (HRIs)
@@ -219,8 +245,7 @@ ax6 = plt.subplot(7, 1, 6, sharex = ax1)
 plt.title('HR Positive Change')
 ax6.plot(time_ppg, hri_list)
 plt.ylabel('bpm')
-plt.ylim(min(hri_list[1500:]), max(hri_list[1500:]))
-plt.legend()
+plt.ylim(min(hri_list[recording_start*fs:]), max(hri_list[recording_start*fs:]))
 plt.grid(True, alpha = 0.5)
 
 #==============================================================================
@@ -231,17 +256,19 @@ plt.grid(True, alpha = 0.5)
 max_pulse_score = 1.5  # 30% weightage
 max_epc_score = 3.5  # 70% weightage
 
-# calculates scores
+# calculates scores (using maximum values in calibration period)
 pulse_scores = hri_list/max(hri_list) * max_pulse_score
 epc_scores = epc_list/max(epc_list) * max_epc_score
 scores = pulse_scores + epc_scores
 
-# plots IBIs    
+# plots scores    
 ax7 = plt.subplot(7, 1, 7, sharex = ax1)
 plt.title('Scores')
 #ax7.plot(time_ppg, pulse_scores, label='Pulse Score')
 #ax7.plot(time_eda, epc_scores, label='EPC Score')
-ax7.plot(time_eda, scores, label='Overall Score')
+#ax7.plot(time_eda, scores, label='Overall Score')
+ax7.scatter(time_eda, scores, s = 1, c=cm.rainbow(np.abs(scores)/scores.max()), edgecolor='none')
+#ax7.scatter(time_eda,scores, s = 0.75, c=cm.cool(np.abs(scores)/scores.max()), edgecolor='none')
 plt.ylabel('score')
 #plt.legend()
 plt.grid(True, alpha = 0.5)
